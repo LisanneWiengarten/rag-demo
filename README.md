96764e1456f1a934ee5af7613df604a4a5030b24
LWiengarten <lisanne.wiengarten@jtl-software.com>
2025-03-17 17:49:20 +0100

    cleanup and fix requirements

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Setup
-- Copy relevant pdf files into data/pdfs. For simplicity, input pdfs are always expected at this location
+- Copy relevant pdf files into the `data` folder. For simplicity, input pdfs are always expected at this location
 - Create a virtual environment for this service e.g. `conda create -n rag python=3.11` and activate it: `conda 
 activate rag`
 - Install the requirements: `pip install -r requirements.txt`
+- Download spacy model: `python -m spacy download de_core_news_lg`
 - Set your environment variables either in the terminal or in the [.env file](deployment/.env) (
   easiest is to copy the [example](deployment/.env.example), rename it and change the values.
-- finally, run the script
+- finally, run the script `python main.py --path path/to/output_dir --create` to create a new index at the given 
+  path or `python main.py --path path/to/output_dir --load` to load an existing index from the given path
 
 
 # Further TODOs
@@ -13,12 +15,13 @@ activate rag`
 - Logging
 - Versioning
 - Deployment
-- Better user interface
+- Better user interface (e.g. allow user to type in questions)
 - Better error handling
-- Better prompts
-- Better data cleaning
-- Better chunking
-- Better answers (more elaborate, include sources)
+- Better prompts (for OCR and answer generation)
+- Better data cleaning (e.g. remove all headers and footers)
+- Better chunking (e.g. consider paragraph numbers)
+- Tweak answers (e.g. more elaborate, include sources)
 - Better data storage (e.g. save separate pages)
-- More flexible LLM/OCR usage
+- More flexible LLM/OCR usage (e.g. easy option to use different vision model)
 - More flexible methods (accept/return more basic types instead of nested dictionaries)
+- Don't hardcode directories/file names (e.g. make it at least a constant)
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from chunking import split_texts_into_chunks
 from pdf_extraction import extract_text_from_folder
 from retrieval import load_vector_store_index, create_vector_store_index
 
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Small RAG Demo")
     parser.add_argument("--create", action="store_true", help="Create a vector store for answering questions")
@@ -19,8 +21,11 @@ if __name__ == "__main__":
 
     if args.create:
         # Extract text from pdfs, split into chunks, create vector store and save it
-        extracted_texts = extract_text_from_folder("data/pdfs")
+        extracted_texts = extract_text_from_folder("data")
         extracted_texts_and_chunks = split_texts_into_chunks(extracted_texts)
+        # Optional: Save intermediate step to json
+        with open("data/extracted_data.json", "w", encoding="utf-8") as file:
+            json.dump(extracted_texts_and_chunks, file)
         index = create_vector_store_index(persist_dir=args.path, chunks=extracted_texts_and_chunks)
 
     elif args.load:
@@ -35,15 +40,25 @@ if __name__ == "__main__":
     query_engine = index.as_query_engine(
         llm=OpenAI(model="gpt-4o"),
         system_prompt="Du bist ein hilfreicher Assistent. Antworte präzise und mit klaren Erklärungen. "
-                      "Falls möglich, nenne Quellen aus den Dokumenten. Antworte immer auf deutsch.")
+                      "Antworte immer auf deutsch.")
 
     response1 = query_engine.query("Wie hoch ist die Grundzulage?")
-    print(response1.response)
+    print(response1.response + "\n\n")
 
     response2 = query_engine.query("Wie werden Versorgungsleistungen aus einer Direktzusage oder einer "
                                    "Unterstützungskasse steuerlich behandelt?")
-    print(response2.response)
+    print(response2.response + "\n\n")
 
     response3 = query_engine.query("Wie werden Leistungen aus einer Direktversicherung, Pensionskasse oder einem "
                                   "Pensionsfonds in der Auszahlungsphase besteuert?")
-    print(response3.response)
+    # For this one, we add our sources to the response
+    retrieved_chunks = []
+    for node in response3.source_nodes:
+        chunk_text = node.node.text
+        chunk_metadata = node.node.metadata
+        retrieved_chunks.append(f"Quelle: {chunk_metadata.get('source', 'Unknown')} "
+                                f"(Chunk {chunk_metadata.get('chunk_index', 'N/A')}):\n\n{chunk_text}\n\n")
+
+    # Format final response
+    response_w_sources = f"Antwort:\n{response3.response}\n\nZitierte Quellen:\n" + "\n---\n".join(retrieved_chunks)
+    print(response_w_sources)
